e65f2e15679484940e825690a5d683c534aeb058
Acrofil <axpeshev@gmail.com>
2023-12-20 19:54:26 +0200

    Work on responsivenes

--- admin-dashboard.js
+++ admin-dashboard.js
@@ -7,26 +7,23 @@ let isOpen = false;
 menuBtn.addEventListener("click", () => {
 
     if (!isOpen) {
-
-        collapseMenu.style.display = "flex";
         
         menuBtn.classList.remove('mdi-menu-open');
         menuBtn.classList.add('mdi-menu-close');
+        collapseMenu.classList.add("active");
         
         isOpen = true;
      
        
     } else if (isOpen) {
-
-        collapseMenu.style.display = 'none';
       
         menuBtn.classList.remove('mdi-menu-close');
         menuBtn.classList.add('mdi-menu-open');
+        collapseMenu.classList.remove("active");
 
         isOpen = false;
     };
 
-    menuBtn.forEach((btnClick) => btnClick.classList.toggle("active"));
 
 
 });
